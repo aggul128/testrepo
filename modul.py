abb1f6592052f9d1ada16ec3dd6b84fc7524bb0b
aggul128 <mehtiyeva.aggul.ogtay.2022@asoiu.edu.az>
2025-04-19 20:32:35 +0400

    Add files via upload

---
+++ modul.py
@@ -0,0 +1,13 @@
+def yoxlayan(a):
+   if a%2 == 0:
+      print ("Eded cutdur")
+
+if __name__ == '__main__':
+   yoxlayan (6)
+   print ('Yoxlayan funk catdirildi')   
+  
+
+#  __name__:Py'de her modül(dosya) yüklendiğinde bu adlı özel bir değişken tanımlanır.
+# Eğer dosya doğrudan çalıştırılmışsa,bu değişken '__main__' olur.
+# Eğer dosya import edilmişse,bu değişkenin değeri,dosyanın ismi(modül) olur.     
+
